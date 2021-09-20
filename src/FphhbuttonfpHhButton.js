d2fe13cdc9d21d65b21d7dd0264708d2747f37cb
frankp018 <89744614+frankp018@users.noreply.github.com>
2021-09-20 18:09:45 -0400

    Add files via upload

--- src/FphhbuttonfpHhButton.js
+++ src/FphhbuttonfpHhButton.js
@@ -3,35 +3,59 @@ import { html, css, LitElement } from 'lit';
 export class FphhbuttonfpHhButton extends LitElement {
   static get styles() {
     return css`
-      :host {
-        display: block;
-        padding: 25px;
-        color: var(--fphhbuttonfp-hh-button-text-color, #000);
-      }
+<<<<<<< HEAD
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>HTML Button Generator</title>
+      <style>
+        button {
+          color: #ffffff;
+          background-color: #2d63c8;
+          font-size: 20px;
+          border: 1px solid #2d63c8;
+          padding: 15px 50px;
+          cursor: pointer
+        }
+        button:hover {
+          color: #2d63c8;
+          background-color: #ffffff;
+        }
+      </style>
+    </head>
+    <body>
+      <button type="button" name="FPHHbutton" autofocus>FPHHbutton</button>
+    </body>
+    </html>
     `;
   }
 
-  static get properties() {
-    return {
-      title: { type: String },
-      counter: { type: Number },
-    };
-  }
-
-  constructor() {
-    super();
-    this.title = 'Hey there';
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
-  }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+    <!DOCTYPE html>
+<html>
+<head>
+	<title>HTML Button Generator</title>
+	<style>
+		button {
+			color: #ffffff;
+			background-color: #2d63c8;
+			font-size: 20px;
+			border: 1px solid #2d63c8;
+			padding: 15px 50px;
+			cursor: pointer
+		}
+		button:hover {
+			color: #2d63c8;
+			background-color: #ffffff;
+		}
+	</style>
+</head>
+<body>
+	<button type="button" name="FPHHbutton" autofocus>FPHHbutton</button>
+</body>
+</html>
     `;
   }
 }
