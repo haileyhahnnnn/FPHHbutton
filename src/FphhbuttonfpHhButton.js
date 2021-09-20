6110c02a00b130a5d674f8f47b271993a8def1ed
frankp018 <frankiepal73@gmail.com>
2021-09-20 17:56:53 -0400

    Fix

--- src/FphhbuttonfpHhButton.js
+++ src/FphhbuttonfpHhButton.js
@@ -59,37 +59,3 @@ export class FphhbuttonfpHhButton extends LitElement {
     `;
   }
 }
-=======
-      :host {
-        display: block;
-        padding: 25px;
-        color: var(--fphhbuttonfp-hh-button-text-color, #000);
-      }
-    `;
-  }
-
-  static get properties() {
-    return {
-      title: { type: String },
-      counter: { type: Number },
-    };
-  }
-
-  constructor() {
-    super();
-    this.title = 'Hey there';
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
-  }
-
-  render() {
-    return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
-    `;
-  }
-}
->>>>>>> ef07439e4dd2b38c5bee087a1b5f3be8d4619600
