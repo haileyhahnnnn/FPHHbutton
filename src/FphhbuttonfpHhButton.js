8836fe4f2a97bbcae6ca6fd815ebf03149412920
Bryan Ollendyke <bto108@psu.edu>
2021-09-21 09:54:35 -0400

    resolve merge conflict

--- src/FphhbuttonfpHhButton.js
+++ src/FphhbuttonfpHhButton.js
@@ -3,12 +3,6 @@ import { html, css, LitElement } from 'lit';
 export class FphhbuttonfpHhButton extends LitElement {
   static get styles() {
     return css`
-<<<<<<< HEAD
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>HTML Button Generator</title>
-      <style>
         button {
           color: #ffffff;
           background-color: #2d63c8;
@@ -21,41 +15,11 @@ export class FphhbuttonfpHhButton extends LitElement {
           color: #2d63c8;
           background-color: #ffffff;
         }
-      </style>
-    </head>
-    <body>
-      <button type="button" name="FPHHbutton" autofocus>FPHHbutton</button>
-    </body>
-    </html>
     `;
   }
 
-
   render() {
     return html`
-    <!DOCTYPE html>
-<html>
-<head>
-	<title>HTML Button Generator</title>
-	<style>
-		button {
-			color: #ffffff;
-			background-color: #2d63c8;
-			font-size: 20px;
-			border: 1px solid #2d63c8;
-			padding: 15px 50px;
-			cursor: pointer
-		}
-		button:hover {
-			color: #2d63c8;
-			background-color: #ffffff;
-		}
-	</style>
-</head>
-<body>
-	<button type="button" name="FPHHbutton" autofocus>FPHHbutton</button>
-</body>
-</html>
-    `;
+<button type="button" name="FPHHbutton" autofocus>FPHHbutton</button>    `;
   }
 }
