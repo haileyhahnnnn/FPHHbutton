cb46f258531b84bad7036589803571242efcb741
frankp018 <frankiepal73@gmail.com>
2021-09-23 14:05:28 -0400

    import packages

--- src/FphhbuttonfpHhButton.js
+++ src/FphhbuttonfpHhButton.js
@@ -1,4 +1,7 @@
 import { html, css, LitElement } from 'lit';
+import "@lrnwebcomponents/simple-icon/lib/simple-icon-lite.js";
+import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
+
 
 export class FphhbuttonfpHhButton extends LitElement {
   static get styles() {
