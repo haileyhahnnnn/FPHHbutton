9f62b6cdbca8d452641185bdd4a9cdc83d3da32e
frankp018 <frankiepal73@gmail.com>
2021-09-26 22:10:58 -0400

    apple button

--- src/FphhbuttonfpHhButton.js
+++ src/FphhbuttonfpHhButton.js
@@ -6,64 +6,58 @@ export class FphhbuttonfpHhButton extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: inline-block;
-        border-radius: 20px;
-        padding: 22px;
-        --apple-foreground-color: red;
-        --apple-background-color: white;
+        display: block;
+        padding: 25px;
+        color: var(--call-to-action-button, #000);
       }
-      .FphhbuttonfpHhButton {
-        border-radius: 20px;
-        padding: 12px 50px;
-        color: var(--apple-foreground-color);
-        background-color: var(--apple-background-color);
-        border: 2px solid var(--apple-foreground-color);
-        transition-duration: 0.5s;
-        font-family: impact;
-        font-size: large;
+      a {
+        border-radius: 30px;
+        border-width: 1px;
+        text-decoration: none;
+        background-color: lightgrey;
+        padding: 15px 25px 15px 25px;
+        font-size: 20px;
+        color: black;
+        transition: 0.2s;
       }
-      .FphhbuttonfpHhButton:hover,
-      :focus {
-        border-radius: 24px;
-        --apple-foreground-color: red;
-        --apple-background-color: white;
-        box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
-          0 17px 50px 0 rgba(0, 0, 0, 0.19);
-      }
-      :host([invert]) .FphhbuttonfpHhButton:hover,
-      :host([invert]) .FphhbuttonfpHhButton:focus {
-        --apple-foreground-color: blue;
-        --apple-background-color: white;
+      a:hover, a:focus {
+        color: white;
+        background-color: green;
+        transition: 0.2s;
       }
     `;
-    }
-// defining all the variables 
+  }
+
   static get properties() {
     return {
+      link: { type: String },
       title: { type: String },
-      invert: {type, Boolean, reflect: true},
-      icon: {type: String},
-      link: {type: String},
-      counter: { type: Number },
+      icon: { type: String },
     };
   }
 
-//constuctor for title 
-constructor () { 
-  super() 
-  this.title = "Apple Button";
-  this.invert = true;
-  this.icon = 'image: music-note';
-  this.link = 'https://www.apple.com/apple-music/';
-}
+  constructor() {
+    super();
+    this.link = 'https://www.apple.com/apple-music/';
+    this.title = 'CLick for Apple';
+    this.icon = false;
+  }
 
-//directing the button 
+  __click(e) {
+    if (this.editMode) {
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+    }
+  }
+
   render() {
     return html`
-    <a href="${this.link}" @click="${this.link}">
-    <button class="FphhbuttonfpHhButton">
-      <simple-icon-lite icon="${this.icon}"></simple-icon-lite>${this.title}
-    </button>
-  </a>
-}
+      <a href="${this.link}" @click=${this.__click} target="_blank" rel="noopener">
+        ${this.icon ? html`<simple-icon-lite icon="${this.icon}"></simple-icon-lite>` : ``}
+        ${this.title}
+        <slot></slot>
+      </a>
+    `;
+  }
 }
