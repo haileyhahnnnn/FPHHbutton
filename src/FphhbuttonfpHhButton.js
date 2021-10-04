f6b3b9886257f94916c072219aef2871b0d86dad
frankp018 <frankiepal73@gmail.com>
2021-10-03 23:34:53 -0400

    update

--- src/FphhbuttonfpHhButton.js
+++ src/FphhbuttonfpHhButton.js
@@ -11,17 +11,18 @@ export class FphhbuttonfpHhButton extends LitElement {
         padding: 20px;
         color: var(--call-to-action-button, #000);
       }
+
       a {
         border-radius: 25px;
         border-width: 2px;
         text-decoration: white underline;
-        background-color: red;
+        background-color: Green;
         padding: 10px 20px 10px 20px;
         font-size: 20px;
         color: black;
         transition: 0.2s;
       }
-      a:hover{ 
+      a:hover {
         background-color: white;
         font-weight: bold;
       }
@@ -29,7 +30,11 @@ export class FphhbuttonfpHhButton extends LitElement {
         color: white;
         background-color: black;
         transition: 0.2s;
-      }  
+      }
+      a: active {
+        background-color: blue;
+        color: red;
+      }
     `;
   }
 
@@ -39,38 +44,47 @@ export class FphhbuttonfpHhButton extends LitElement {
       title: { type: String },
       icon: { type: String },
       disabled: { type: Boolean, reflect: true },
+      play: { type: Boolean, reflect: true },
     };
   }
 
   constructor() {
     super();
+    this.clicked = false;
     this.link = 'https://www.apple.com/apple-music/';
-    this.title = 'CLick for Apple';
+    this.title = 'Click For Apple';
     this.icon = 'save';
     this.disabled = false;
     this.addEventListener = 'CTAButton';
   }
+
   __click(e) {
     if (this.editMode) {
       e.preventDefault();
       e.stopPropagation();
       e.stopImmediatePropagation();
+      this.play = true;
     }
   }
 
   render() {
     return html`
+      <div style="display: flex; justify-content: center;">
+      
       <a
         href="${this.link}"
         @click=${this.__click}
         target="_blank"
         rel="noopener"
       >
-        ${this.icon
-          ? html`<simple-icon-lite icon="${this.icon}"></simple-icon-lite>`
-          : ``}
+        ${
+          this.icon
+            ? html`<simple-icon-lite icon="${this.icon}"></simple-icon-lite>`
+            : ``
+        }
         ${this.title}
         <slot></slot>
+        </div>
       </a>
     `;
   }
